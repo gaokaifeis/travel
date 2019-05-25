62665e117bc5cf251146cd2824dd0f7367936a3c
GKF <gaokf0103@163.com>
2019-05-25 19:22:57 +0800

    Weekend component OK

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,5 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
 <style>
 </style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <p class="item-price"></p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/52/b6776b49d27025.jpg_r_640x214_35ae205c.jpg',
+          title: '美丽普陀山',
+          desc: '人间仙境杭州，佛教圣地普陀山等你来玩！在这里静心接收佛家的洗礼，用心感受佛文化'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1509/2f/161295e0b5b915.jpg_r_640x214_e2857d1b.jpg',
+          title: '湖州必游',
+          desc: '这里是江南古城，这里是鱼米之乡'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/99/20ec55b6c6f7f8.jpg_r_640x214_559590a6.jpg',
+          title: '探寻湖州文化古迹',
+          desc: '品湖州历史之韵'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    line-height: .6rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    min-width: 0
+    .item-title
+      margin-top: .26rem
+      line-height: .44rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
